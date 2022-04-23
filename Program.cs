13fb2811e91017ffa57917fbd28c35f65886a646
Dirk Kok <the@dirkkok.nl>
2022-04-23 22:07:17 +0200

    fixed

--- Program.cs
+++ Program.cs
@@ -9,23 +9,26 @@ var discord = new DiscordClient(new DiscordConfiguration() {
 	Token = Environment.GetEnvironmentVariable("BOT_TOKEN")
 });
 
-VoiceNextExtension voicenextExtension = discord.UseVoiceNext();
+VoiceNextExtension voiceNextExtension = discord.UseVoiceNext();
 
 ulong channelId = ulong.Parse(Environment.GetEnvironmentVariable("CHANNEL_ID"));
 string track = Environment.GetEnvironmentVariable("TRACK");
 
 discord.Ready += (o, e) => {
 	_ = Task.Run(async () => {
-		DiscordChannel channel = await discord.GetChannelAsync(channelId);
-		while (true) {
-			await Task.Delay(TimeSpan.FromSeconds(1)); // prevents an error (idk)
-			using VoiceNextConnection connection = voicenextExtension.GetConnection(channel.Guild) ?? await channel.ConnectAsync();
-			await using Stream pcm = File.OpenRead(track);
+		try {
+			await Task.Delay(TimeSpan.FromSeconds(1)); // prevents an NRE inside ConnectAsync (yeah)
+			DiscordChannel channel = await discord.GetChannelAsync(channelId);
+			using VoiceNextConnection connection = voiceNextExtension.GetConnection(channel.Guild) ?? await channel.ConnectAsync();
 			using VoiceTransmitSink transmit = connection.GetTransmitSink();
+
+			await using FileStream pcm = File.OpenRead(track);
 			while (true) {
+				pcm.Seek(0, SeekOrigin.Begin);
 				await pcm.CopyToAsync(transmit, 256 * transmit.SampleLength);
-				pcm.Position = 0;
 			}
+		} catch (Exception ex) {
+			Console.WriteLine(ex);
 		}
 	});
 	
