4f7628d1e5a1d2f0ed8a534cef296cf9d92a3b7c
Dirk Kok <the@dirkkok.nl>
2022-04-23 21:33:32 +0200

    use .net 6.0; remove unneeded dependencies; use top-level statements

--- Program.cs
+++ Program.cs
@@ -5,39 +5,33 @@ using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.VoiceNext;
 
-namespace IkIheMusicBotSimplified {
-	public sealed class Program {
-		private static async Task Main(string[] args) {
-			var discord = new DiscordClient(new DiscordConfiguration() {
-				Token = Environment.GetEnvironmentVariable("BOT_TOKEN")
-			});
+var discord = new DiscordClient(new DiscordConfiguration() {
+	Token = Environment.GetEnvironmentVariable("BOT_TOKEN")
+});
 
-			ulong channelId = ulong.Parse(Environment.GetEnvironmentVariable("CHANNEL_ID"));
-			string track = Environment.GetEnvironmentVariable("TRACK");
-			
-			discord.Ready += async (o, e) => {
-				StartPlaying(await discord.GetChannelAsync(channelId), track, discord.GetVoiceNext());
-			};
+VoiceNextExtension voicenextExtension = discord.UseVoiceNext();
 
-			discord.UseVoiceNext();
-			await discord.ConnectAsync();
+ulong channelId = ulong.Parse(Environment.GetEnvironmentVariable("CHANNEL_ID"));
+string track = Environment.GetEnvironmentVariable("TRACK");
 
-			await Task.Delay(-1);
+discord.Ready += (o, e) => {
+	_ = Task.Run(async () => {
+		DiscordChannel channel = await discord.GetChannelAsync(channelId);
+		while (true) {
+			await Task.Delay(TimeSpan.FromSeconds(1)); // prevents an error (idk)
+			using VoiceNextConnection connection = voicenextExtension.GetConnection(channel.Guild) ?? await channel.ConnectAsync();
+			await using Stream pcm = File.OpenRead(track);
+			using VoiceTransmitSink transmit = connection.GetTransmitSink();
+			while (true) {
+				await pcm.CopyToAsync(transmit, 256 * transmit.SampleLength);
+				pcm.Position = 0;
+			}
 		}
+	});
+	
+	return Task.CompletedTask;
+};
 
-		private static void StartPlaying(DiscordChannel channel, string track, VoiceNextExtension voiceNextExtension) {
-			_ = Task.Run(async () => {
-				while (true) {
-					await Task.Delay(TimeSpan.FromSeconds(1)); // prevents an error (idk)
-					using VoiceNextConnection connection = voiceNextExtension.GetConnection(channel.Guild) ?? await channel.ConnectAsync();
-					await using Stream pcm = File.OpenRead(track);
-					using VoiceTransmitSink transmit = connection.GetTransmitSink();
-					while (true) {
-						await pcm.CopyToAsync(transmit, 256 * transmit.SampleLength);
-						pcm.Position = 0;
-					}
-				}
-			});
-		}
-	}
-}
+await discord.ConnectAsync();
+
+await Task.Delay(-1);
