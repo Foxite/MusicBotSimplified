5208a3c83b2ecce7c2fe054855abddb7780ca28f
Dirk Kok <the@dirkkok.nl>
2022-05-11 19:21:01 +0200

    go back to dsharpplus because the ~~clowns~~ devs of discord.net still have not fixed the disconnect problem that was plaguing roosterbot 4 years ago

--- Program.cs
+++ Program.cs
@@ -17,71 +17,81 @@ var discord = new DiscordClient(new DiscordConfiguration() {
 
 var stopProgram = new CancellationTokenSource();
 
-discord.Ready += () => {
-	_ = Task.Run(async () => {
-		DiscordChannel channel = await discord.GetChannelAsync(channelId);
+discord.Ready += (_, _) => {
+	Task.Run(async () => {
+		try {
+			await Task.Delay(TimeSpan.FromSeconds(1));
+			DiscordChannel channel = await discord.GetChannelAsync(channelId);
 
-		// no using, this function returns shortly.
-		VoiceNextConnection audio = await channel.ConnectAsync();
+			// no using, this function returns shortly.
+			VoiceNextConnection audio = await channel.ConnectAsync();
 
-		int users = 0;
+			int users = 0;
 
-		bool keepTransmitting = true;
-		long position = 0;
+			bool keepTransmitting = true;
+			long position = 0;
 
-		void StopTransmitting() {
-			keepTransmitting = false;
-		}
-
-		byte[] buffer = new byte[4096];
-		void StartTransmitting() {
-			keepTransmitting = true;
-			_ = Task.Run(async () => {
-				try {
-					await using FileStream file = File.OpenRead(track);
-					VoiceTransmitSink transmit = audio.GetTransmitSink();
-					while (keepTransmitting) {
-						file.Seek(position, SeekOrigin.Begin);
-						int count;
-						while (keepTransmitting && (count = file.Read(buffer, 0, buffer.Length)) > 0) {
-							await transmit.WriteAsync(buffer, 0, count);
-						}
-						if (keepTransmitting) {
-							position = 0;
-						} else {
-							break;
-						}
-					}
-					position = file.Position;
-				} catch (Exception ex) {
-					Console.WriteLine(ex);
-					stopProgram.Cancel();
-				}
-			});
-		}
-
-		discord.VoiceStateUpdated += (_, eventArgs) => {
-			if (eventArgs.After.Channel != null && eventArgs.After.Channel.Id == channelId) {
-				if (users == 0) {
-					StartTransmitting();
-				}
-				users++;
-			} else if (eventArgs.Before.Channel != null && eventArgs.Before.Channel.Id == channelId) {
-				users--;
-				if (users == 0) {
-					StopTransmitting();
-				}
+			void StopTransmitting() {
+				keepTransmitting = false;
 			}
-			return Task.CompletedTask;
-		};
-		
-		users = audio.GetStreams().Count;
-		if (users > 0) {
-			StartTransmitting();
+
+			byte[] buffer = new byte[4096];
+
+			void StartTransmitting() {
+				keepTransmitting = true;
+				_ = Task.Run(async () => {
+					try {
+						await using FileStream file = File.OpenRead(track);
+						VoiceTransmitSink transmit = audio.GetTransmitSink();
+						while (keepTransmitting) {
+							file.Seek(position, SeekOrigin.Begin);
+							int count;
+							while (keepTransmitting && (count = file.Read(buffer, 0, buffer.Length)) > 0) {
+								await transmit.WriteAsync(buffer, 0, count);
+							}
+
+							if (keepTransmitting) {
+								position = 0;
+							} else {
+								break;
+							}
+						}
+
+						position = file.Position;
+					} catch (Exception ex) {
+						Console.WriteLine(ex);
+						stopProgram.Cancel();
+					}
+				});
+			}
+
+			discord.VoiceStateUpdated += (_, e) => {
+				if (e.After.Channel != null && e.After.Channel.Id == channelId) {
+					users++;
+					if (users > 1) {
+						StartTransmitting();
+					}
+				} else if (e.Before.Channel != null && e.Before.Channel.Id == channelId) {
+					users--;
+					if (users <= 1) {
+						StopTransmitting();
+					}
+				}
+				return Task.CompletedTask;
+			};
+
+			users = audio.TargetChannel.Users.Count;
+			if (users > 1) {
+				StartTransmitting();
+			}
+		} catch (Exception ex) {
+			Console.WriteLine(ex);
 		}
 	});
 	return Task.CompletedTask;
 };
+
+discord.UseVoiceNext();
 
 await discord.ConnectAsync();
 
